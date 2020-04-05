8c62bfb6e85d66a2129f594ea676482f3ae95235
Piotr Piątyszek <piotrp@wektor.xyz>
2020-04-05 19:53:58 +0200

    Test travis deploy all branches

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 const StatsPlugin = require('stats-webpack-plugin')
+
 module.exports = {
   chainWebpack: config => config.optimization.minimize(false),
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? (process.env.TRAVIS ? (process.env.TRAVIS_BRANCH === 'master' ? '/' : ('/branch/' + process.env.TRAVIS_BRANCH + '/')) : '/') : '/',
   pluginOptions: {
     webpackBundleAnalyzer: {
       openAnalyzer: false
     }
-  }
+  },
+  outputDir: 'dist'
 }
