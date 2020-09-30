82f1497d2ce06101d4fe5a944657698527d3d3b2
Piotr Piątyszek <piotrp@wektor.xyz>
2020-10-01 01:32:13 +0200

    Update examples

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -2,18 +2,18 @@ export default {
   examples: [
     {
       name: 'Warsaw apartments',
-      url: 'https://gist.githubusercontent.com/piotrpiatyszek/fe8fcce96ebd46012a9e53aa3910fc69/raw/39e488d1299e763a29c083395b4c7da8fde62c23/data.json',
-      session: 'https://gist.githubusercontent.com/piotrpiatyszek/af2f1c4ad85bc48ca8641d9b31f73dfc/raw/71a74f086c7576f912f469e5d910c18eec847d3e/session.json'
+      url: 'https://gist.githubusercontent.com/piotrpiatyszek/3bd276c312af555c9f19cfd524b52c01/raw/63c6d7dbad270c10cafa8a252cecda21b7abe54c/data.json',
+      session: 'https://gist.githubusercontent.com/piotrpiatyszek/af2f1c4ad85bc48ca8641d9b31f73dfc/raw/2d832406e63a55ed9741da4e5aedbe07953e731b/session.json'
     },
     {
       name: 'FIFA Players Value',
-      url: 'https://gist.githubusercontent.com/piotrpiatyszek/adb844f8f353502548bc335a3f28bcf2/raw/b1268cf8c791a9ce9aaadfeef3a7c0aeaa8d7d75/data.json',
-      session: 'https://gist.githubusercontent.com/piotrpiatyszek/fca4ca239036f5d946a6b90fe92db818/raw/4b9eb4c305798d4e2dd08111f0ca2f0b8db6e7c5/session.json'
+      url: 'https://gist.githubusercontent.com/piotrpiatyszek/21c4380a44d4e527b47ba0a4f16b18f5/raw/0d5e2d98890162d631d70a93d76604533a183ee1/data.json',
+      session: 'https://gist.githubusercontent.com/piotrpiatyszek/e5872fb4c62a2600e30393a66ff3413a/raw/a381762b5af4cc9dcd7d4f119013b33d8b92848d/session.json'
     },
     {
       name: 'Employees status classification',
-      url: 'https://gist.githubusercontent.com/piotrpiatyszek/82b6eadfd1d932ec79ad783ae74bf1a2/raw/cfaadd9752d7cfeb62766910a9e4c1099fe466c8/data.json',
-      session: 'https://gist.githubusercontent.com/piotrpiatyszek/0765dfca6af874494eeda8e53ffb812d/raw/03707b3dc04024911d9d3dc4883e581f6909bef3/session.json'
+      url: 'https://gist.githubusercontent.com/piotrpiatyszek/c7786a137e85695d9183f23f7223ebb6/raw/8163e7b39f0f400a6d4b8f9c1bdffbb5da80fb7d/data.json',
+      session: 'https://gist.githubusercontent.com/piotrpiatyszek/97fabafba2c20b9f0ab6c2c691eb17cf/raw/a023546e95fbddbe6d6edbe5c9f60f3204452eb7/session.json'
     }
   ],
   url: 'https://arena.drwhy.ai',
