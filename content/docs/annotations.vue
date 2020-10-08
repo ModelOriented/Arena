fbb398f7716952358b58ba12c86c5546a7212438
Piotr Piątyszek <piotrp@wektor.xyz>
2020-10-08 22:54:16 +0200

    Page annotations

---
+++ content/docs/annotations.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="content">
+    <DocSection name="Annotate using brush">
+      <Player video-id="466309501" />
+      This mechanism allows you to draw on charts and in the space between them.
+      You can use different colors and sizes to annotate inconspicuous figures and write comments.
+      The drawing area is located over the workspace.
+      If you move the chart, the annotation stays in the old location.<br>
+      To change brush size, use the mouse scroll.
+      Rubber is the last available color.
+    </DocSection>
+  </div>
+</template>
